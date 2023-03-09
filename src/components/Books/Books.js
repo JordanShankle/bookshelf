29b9b4d054c237eb76518379953ebfc0325347ad
Jordan Shankle <ujshankle@gmail.com>
2023-03-09 12:04:33 -0600

    Add styling to book cards on mobile; Add images to json.

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,3 +1,6 @@
+// Sass
+import './Books.scss'
+
 const Books = ({ books }) => {
 
 
@@ -5,17 +8,20 @@ const Books = ({ books }) => {
         <>
             {
                 books.map((book) => {
-                    const { id, title, author, category, pages, summary, rating } = book;
+                    const { id, image, title, author, category, pages, summary, rating } = book;
 
                     return (
                         <article key={id} className='book'>
 
-                            <h3>{title}</h3>
-                            <p>By: {author}</p>
-                            <p>Genre: {category}</p>
-                            <p>Pages: {pages}</p>
-                            <p>Rating: {rating}</p>
-                            <p>{summary}</p>
+                            <h3 className="book__title" >{title}</h3>
+                            <div className="book__image__container">
+                                <img src={image} alt="book cover" className='book__image' />
+                            </div>
+                            <span className="book__author" >By: {author}</span>
+                            <span className="book__category" >Genre: {category}</span>
+                            <span className="book__pages" >Pages: {pages}</span>
+                            <span className="book__rating" >Rating: {rating}</span>
+                            <span className="book__summary" >{summary}</span>
 
                         </article>
                     )
